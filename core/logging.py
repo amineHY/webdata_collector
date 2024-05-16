e5307fa1601322680de318600c621e470635a688
Amine Hadj-Youcef <hadjyoucef.amine@gmail.com>
2024-05-16 22:41:16 +0200

    update logger

--- core/logging.py
+++ core/logging.py
@@ -3,7 +3,8 @@ import logging
 
 def setup_logging():
     logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s",
     )
     logger = logging.getLogger(__name__)
     return logger
