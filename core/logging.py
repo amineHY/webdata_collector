49b8480f473fed88146950fdb8adc8f288f0adcf
Amine Hadj-Youcef <hadjyoucef.amine@gmail.com>
2024-05-17 09:06:36 +0200

    add docstring

--- core/logging.py
+++ core/logging.py
@@ -2,12 +2,19 @@ import logging
 
 
 def setup_logging():
+    """
+    Sets up a basic logging configuration and returns a logger instance.
+
+    The logging level is set to INFO. The format string is customized to include
+    timestamp, log level, filename, function name, and the actual log message.
+
+    :return: A logger instance for the current module.
+    """
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s",
     )
-    logger = logging.getLogger(__name__)
-    return logger
+    return logging.getLogger(__name__)
 
 
 logger = setup_logging()
